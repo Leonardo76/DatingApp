c107b5b008ae69cb4acaf0f0b9d734fdf4671747
leonardo76 <zgabearta.iftode2015@gmail.com>
2018-10-29 12:58:04 +0200

    Initial commit

---
+++ DatingApp.API/DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// <para>
+        /// Initializes a new instance of the <see cref="DataContext" /> class using the specified options.
+        /// The <see cref="M:Microsoft.EntityFrameworkCore.DbContext.OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder)" /> method will still be called to allow further
+        /// configuration of the options.
+        /// </para>
+        /// </summary>
+        /// <param name="options">The options for this context.</param>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
